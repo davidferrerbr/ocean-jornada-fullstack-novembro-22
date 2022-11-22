7b757e46e18d81a7a8de0754e95983297b8e9c3c
David Ferrer <53544263+davidferrerbr@users.noreply.github.com>
2022-11-22 19:28:45 -0300

    add ano no Footer

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,12 +1,12 @@
 import "./Footer.css"
 
 function Footer() {
+    const year = new Date().getFullYear();
     return(
         <div className="Footer">
-            Direitos Reservados para David Ferrer
+            Todos os direitos Reservados ~ {year}.
         </div>
     )
-
 }
 
 export default Footer;
