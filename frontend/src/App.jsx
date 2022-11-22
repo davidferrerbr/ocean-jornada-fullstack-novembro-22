be2fcabbc425d95b4dba172c904813aac3c73cdc
David Ferrer <53544263+davidferrerbr@users.noreply.github.com>
2022-11-21 22:09:15 -0300

    Create Footer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,17 @@
 import './App.css'
 import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
 
 function App() {
   return (
     <div className="App">
       <Header />
-      
+
       <div className="content">
         Conteúdo da página
       </div>
+
+      <Footer />
     </div>
   )
 }
---
+++ frontend/src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import "./Footer.css"
+
+function Footer() {
+    return(
+        <div className="Footer">
+            Direitos Reservados para David Ferrer
+        </div>
+    )
+
+}
+
+export default Footer;
