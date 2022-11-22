58e1ecfe9383758ff044f8dca0aca285e31598e6
David Ferrer <53544263+davidferrerbr@users.noreply.github.com>
2022-11-22 20:00:30 -0300

    Create Routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,11 @@
-import './App.css'
+import { Route, Routes } from "react-router-dom"
+
 import Header from './components/Header/Header'
 import Footer from './components/Footer/Footer'
+
+import './App.css'
+import ReadAll from "./components/ReadAll/ReadAll"
+import ItemCard from "./components/ItemCard/ItemCard"
 
 function App() {
   return (
@@ -8,7 +13,11 @@ function App() {
       <Header />
 
       <div className="content">
-        Conteúdo da página
+        <Routes>
+          <Route path="/" element={<ReadAll />} />
+
+          <Route path="/adicionar" element={<ItemCard />} />
+        </Routes>
       </div>
 
       <Footer />
