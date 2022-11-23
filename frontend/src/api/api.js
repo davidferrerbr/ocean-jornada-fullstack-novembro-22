8cfa9c7bd65b7332d778c42ae46d9f5d7c3cf9b7
David Ferrer <53544263+davidferrerbr@users.noreply.github.com>
2022-11-23 20:08:46 -0300

    Create da `api.js`

---
+++ frontend/src/api/api.js
@@ -0,0 +1,21 @@
+export const Api = {
+    baseUrl: "http://127.0.0.1:3000/",
+
+    // ENDPOINTS DE ITENS
+    itens:{
+        endpoint: () => {
+            return Api.baseUrl + "itens";
+        },
+        readAll: () => {
+            return this.endpoint() + "/";
+        },
+    },
+
+    // MONTAR AS REQUISIÇÕES
+    // GET
+    buildApiGetRequest: (url) => {
+        return fetch(url, {
+            method: "GET",
+        });
+    },
+};
