59f4f1badc53f1772b8221dc4ecc1e654d3e5166
David Ferrer <53544263+davidferrerbr@users.noreply.github.com>
2022-11-23 21:35:15 -0300

    implemetação do `ReadAll` com a `api`

--- frontend/src/components/ReadAll/ReadAll.jsx
+++ frontend/src/components/ReadAll/ReadAll.jsx
@@ -1,29 +1,49 @@
+import { useEffect, useState } from "react";
 import { Api } from "../../api/api";
 import ItemCard from "../ItemCard/ItemCard";
 import "./ReadAll.css"
 
 function ReadAll() {
-    const itens = [
-        {
-            _id: "1234", 
-            nome: "Item 1",
-            imagemUri: "https://picsum.photos/201/201" 
-        },
-        {
-            _id: "9999", 
-            nome: "Item 2",
-            imagemUri: "https://picsum.photos/200/200"
-        },
-        {
-            _id: "5555", 
-            nome: "Item 3",
-            imagemUri: "https://picsum.photos/202/202"
-        }
-    ];
+    // const itens = [
+    //     {
+    //         _id: "1234", 
+    //         nome: "Item 1",
+    //         imagemUrl: "https://picsum.photos/201/201" 
+    //     },
+    //     {
+    //         _id: "9999", 
+    //         nome: "Item 2",
+    //         imagemUrl: "https://picsum.photos/200/200"
+    //     },
+    //     {
+    //         _id: "5555", 
+    //         nome: "Item 3",
+    //         imagemUrl: "https://picsum.photos/202/202"
+    //     }
+    // ];
 
-    const readAllUrl = Api.itens.readAll();
-    console.log(readAllUrl);
-    Api.buildApiGetRequest(readAllUrl);
+    // DESCONTRUÇÃO DE ARRAYS
+    const [itens, setItens] = useState();
+
+    async function realizarRequest() {
+        const readAllUrl = Api.itens.readAll();
+
+        const response = await Api.buildApiGetRequest(readAllUrl);
+
+        const result = await response.json();
+        console.log(result);
+
+        setItens(result);
+    }
+
+    useEffect(() => {
+        realizarRequest();
+    }, []);
+    
+
+    if (itens === undefined) {
+        return <div>Carregando...</div>;
+    }
 
     return (
         <div className="ReadAll">
