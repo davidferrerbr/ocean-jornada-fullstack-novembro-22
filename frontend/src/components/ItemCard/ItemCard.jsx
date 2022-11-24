574081ea19dcea1fe99a343ed131f3bee502a69b
David Ferrer <53544263+davidferrerbr@users.noreply.github.com>
2022-11-23 22:20:10 -0300

    ajuste de imagem da `ItemCard`

--- frontend/src/components/ItemCard/ItemCard.jsx
+++ frontend/src/components/ItemCard/ItemCard.jsx
@@ -6,7 +6,7 @@ function ItemCard(props){
     return (
         <div className="ItemCard">
             <h1>{item.nome}</h1>
-            <img src={item.imagemUrl} alt={`Imagem do Item ${item.nome}`} width={200} />
+            <img src={item.imagemUrl} alt={`Imagem do Item ${item.nome}`} width={200} height={200} />
         </div>
     )
 }
